55ee2c89e6c1f342be75ed0fb4b41b1e20b0d425
Ashraf-Ebrahime <ashrfaha29@gmail.com>
2025-04-30 02:15:19 +0300

    Add files via upload

---
+++ project/profile.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>RESERVATION</title>
+    <link
+      rel="shortcut icon"
+      href="img/4.jpg"
+      type="image/x-icon"
+    />
+  </head>
+  <body style="background-color: white;">
+          <div class="card"> <img class="card-img-top" src="img/4.jpg" alt="Card image cap">
+              <div class="card-body little-profile text-center">
+                  <div class="pro-img"><img src="img/3.jpg" alt="user"></div>
+                  <h3 class="m-b-0">Brad Macullam</h3>
+                  <p>Web Designer &amp; Developer</p> <a href="javascript:void(0)" class="m-t-10 waves-effect waves-dark btn btn-primary btn-md btn-rounded" data-abc="true">Follow</a>
+                  <div class="row text-center m-t-20">
+                      <div class="col-lg-4 col-md-4 m-t-20">
+                          <h3 class="m-b-0 font-light">10434</h3><small>Articles</small>
+                      </div>
+                      <div class="col-lg-4 col-md-4 m-t-20">
+                          <h3 class="m-b-0 font-light">434K</h3><small>Followers</small>
+                      </div>
+                      <div class="col-lg-4 col-md-4 m-t-20">
+                          <h3 class="m-b-0 font-light">5454</h3><small>Following</small>
+                      </div>
+                      </div>
+                  </div>
+              </div>
+
+<div class="container">
+  <div class="row">
+
+  <div class="col-6 offset-3 mt-5 rounded-2 text-center">
+    <h1>share your idea</h1>
+  </div>
+    <div class="col-6 offset-3 mt-5 rounded-2 text-white bg-dark">
+<form action="storePost.php" method="post">
+          <div class="mb-3">
+            <label for="" class="form-label">title</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              class="form-control"
+              placeholder=""
+              aria-describedby="helpId"
+            />
+            <small id="helpId" class="text-muted">Help text</small>
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label">content</label>
+            <textarea
+              type="text"
+              name=""
+              id=""
+              class="form-control"
+              placeholder=""
+              aria-describedby="helpId"
+            ></textarea>
+            <small id="helpId" class="text-muted">Help text</small>
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label">title</label>
+            <input
+              type="file"
+              name=""
+              id=""
+              class="form-control"
+              placeholder=""
+              aria-describedby="helpId"
+            />
+            <small id="helpId" class="text-muted">Help text</small>
+          </div>
+          <button
+            type="submit"
+            class="btn btn-primary my-5"
+          >
+            Submit
+          </button>
+</form>
+        
+    </div>
+  </div>
+
+  
+<div class="col-8 offset-2 mt-5 rounded-2">
+<div class="card">
+  <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
+  <div class="card-body">
+    <h4 class="card-title">Title</h4>
+    <p class="card-text">Text</p>
+  </div>
+</div>
+
+</div>
+</div>
+
+
+
+
+<script src="bootstrap-4.0.0-dist/js/popper.min.js"></script>
+    <script src="bootstrap-4.0.0-dist/js/jquery-3.7.1.min.js"></script>
+    <script src="bootstrap-4.0.0-dist/js/bootstrap.js"></script>
+    <script src="validation form1.js"></script>
+ </body>
+</html>
